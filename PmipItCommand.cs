a97a4b73668570d94ef5bb4d4d7dbbdcbe1106c5
Sebastien Lebreton <sebleb@microsoft.com>
2015-10-13 16:00:04 +0200

    Initial commit

---
+++ PmipItCommand.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Text.RegularExpressions;
+using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+
+namespace PmipIt
+{
+	internal sealed class PmipItCommand
+	{
+		public const int CommandId = 0x0100;
+		public static readonly Guid CommandSet = new Guid("de766436-9607-48b3-b78c-4ab8f6ceb849");
+
+		private readonly PmipItPackage _package;
+
+		private PmipItCommand(PmipItPackage package)
+		{
+			if (package == null)
+				throw new ArgumentNullException();
+
+			_package = package;
+
+			var commandService = ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+			if (commandService == null)
+				return;
+
+			var menuCommandId = new CommandID(CommandSet, CommandId);
+			var menuItem = new MenuCommand(MenuItemCallback, menuCommandId);
+			commandService.AddCommand(menuItem);
+		}
+
+		public static PmipItCommand Instance
+		{
+			get;
+			private set;
+		}
+
+		private IServiceProvider ServiceProvider
+		{
+			get
+			{
+				return _package;
+			}
+		}
+
+		public static void Initialize(PmipItPackage package)
+		{
+			Instance = new PmipItCommand(package);
+		}
+
+		private void MenuItemCallback(object sender, EventArgs e)
+		{
+			var dte = _package.Dte;
+
+			var debugger = dte.Debugger;
+			if (debugger == null)
+				return;
+
+			var thread = dte.Debugger.CurrentThread;
+
+			var frames = thread?.StackFrames;
+			if (frames == null)
+				return;
+
+			_package.OutputWindowDebugPane.Activate();
+
+            foreach (StackFrame frame in frames)
+			{
+				var stackInfo = frame.FunctionName;
+				if (Regex.IsMatch(stackInfo, "\\d+()"))
+				{
+					var expression = $"{{,,mono.dll}}mono_pmip((void*)0x{Regex.Replace(stackInfo, "\\(|\\)", string.Empty)})";
+					var result = debugger.GetExpression(expression, true);
+					stackInfo = result.IsValidValue ? result.Value : string.Concat("Unable to evaluate ", expression);
+				}
+
+				_package.OutputWindowDebugPane.OutputString(stackInfo);
+				_package.OutputWindowDebugPane.OutputString(Environment.NewLine);
+			}
+		}
+	}
+}
---
+++ PmipItPackage.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using EnvDTE;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace PmipIt
+{
+	[PackageRegistration(UseManagedResourcesOnly = true)]
+	[InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+	[ProvideMenuResource("Menus.ctmenu", 1)]
+	[Guid(PackageGuidString)]
+	[SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+	public sealed class PmipItPackage : Package
+	{
+		public const string PackageGuidString = "0aa8493b-87d6-453f-b5c9-89acdb7a3c93";
+
+		public DTE Dte => (DTE)GetService(typeof(DTE));
+		public IVsOutputWindow OutputWindow => GetGlobalService(typeof (SVsOutputWindow)) as IVsOutputWindow;
+
+		public IVsOutputWindowPane OutputWindowDebugPane
+		{
+			get
+			{
+				var debugPaneGuid = VSConstants.GUID_OutWindowDebugPane; 
+				IVsOutputWindowPane debugPane;
+				OutputWindow.GetPane(ref debugPaneGuid, out debugPane);
+				return debugPane;
+			}
+		}
+
+		protected override void Initialize()
+		{
+			PmipItCommand.Initialize(this);
+			base.Initialize();
+		}
+	}
+}
